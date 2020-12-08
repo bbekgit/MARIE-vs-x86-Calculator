13ecbed471a37990e46f38822e3858a9ce452e51
Bibek Gupta <35547667+bbekgit@users.noreply.github.com>
2020-12-07 20:18:13 -0600

    initial calculator template

---
+++ src/cal.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+   int choice,n1,n2,ans;
+   
+   while(1) {
+    
+      printf("\nWelCome To Simple Calcualtor Choose One Option:\n");
+      printf(" 1: Addition\n");
+      printf(" 2: Substraction\n");
+      printf(" 3: Multiplication\n");
+      printf(" 4: Division\n");
+      printf(" 5: Quit\n");
+      printf("Enter your choice:");
+      scanf("%d",&choice);
+     }
+
+exit(0);
+}
